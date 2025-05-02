8b10683259d189ef65dd38b6b3fdf1abf5f2da15
kartikmangukiya <kmtech012@gmail.com>
2025-05-02 23:47:32 +0530

    filter complete

--- ARRAYS/index.js
+++ ARRAYS/index.js
@@ -1,10 +1,12 @@
 // arrays like an object
 // 3 ways to create an array
 // (1)using the array constructor
-// const players=new Array('ram','sita','laxman','bharat');
-// console.log(players)
-// console.log(typeof players)
-// console.log(Array.isArray(players))
+const players=new Array('ram','sita','laxman','bharat');
+console.log(players)
+console.log(typeof players)
+console.log(Array.isArray(players))
+console.log(players.at(-1))     //it return last element in array
+ 
 
 // (2)array literal
 const fruits=['apple','banana','cherry'];
@@ -71,11 +73,11 @@ console.log(myMap);
 // // example of foreach method
 // numbers.forEach((currentele)=>{
 //     console.log(currentele*2);
-// })
+// });
 // // example of map method
 // const myMap=numbers.map((currentele)=>{
 //     return currentele*2;
-// })
+// });
 // console.log(myMap);
 
 const myMethod=["gita","sita","mira","rina","jaya"];
@@ -106,10 +108,48 @@ console.log(myMethod);
 
 // searching & filtering an array
 // (1)indexof(searchElement)- it returns index value of element
-const nummbers=[1,2,3,4,5,6,4,8]
+const nummbers=[1,2,3,4,5,6,4,8];
 console.log(nummbers.indexOf(4)) //3
 // (2)includes(searchElemenet,fromIndex-(search where to start)) -it returns element is exists or not.
 console.log(nummbers.includes(4,3));
 // (3)lastindexof(searchElement)- it returns last reapeted index
 console.log(nummbers.indexOf(4))
 console.log(nummbers.lastIndexOf(4,5)); 
+
+// [ search _ filter ] filter in an array
+
+const posNumbers=[1,2,3,8,5,3,7,9,4,6];
+// [i] find() method - it returns the first matching element in array which you want to find or undefined if no element is found.
+const myFind=posNumbers.find((currentele)=>{
+    return currentele > 7;
+    // return currentele > 89;    undefined
+});
+console.log(myFind)
+console.log(typeof myFind); //output - number
+
+// [ii] findIndex() method - it returns the first matching element index in array which you want to find or -1 if no element is found.
+// if only one argument than no need to parentheses
+const myFindIndex=posNumbers.findIndex(currentele=>{
+    return currentele > 7;
+    // return currentele > 89;    return -1
+});
+console.log(myFindIndex)
+console.log(typeof myFindIndex); //output - number
+
+//[iii] filter() method - it returns new array with all element that return true from the callback , if no element match than return an empty array
+// syntax array.filter(callback(currentEle,index,array));
+
+// we use fat arrow function with one argument
+const myFilter=posNumbers.filter(currentele=> currentele > 5);
+const myFilter1=posNumbers.filter(currentele=> currentele > 52);
+console.log(myFilter);
+console.log(myFilter1); //return empty array bcos not found an element
+
+// example1 - In e-commerce website when we want to delete or remove any product from addtocart page....
+
+const products=[1,2,3,6,4,5,6,7];
+let selectProduct=6;
+
+const result=products.filter(currentele=> currentele!=selectProduct );
+console.log(result);
+
