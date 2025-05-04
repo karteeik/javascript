1b89066db6d74b139371af8c58e06d7a321d6dd1
kartikmangukiya <kmtech012@gmail.com>
2025-05-04 23:19:58 +0530

    string basic and template str

--- STRINGS/index.js
+++ STRINGS/index.js
@@ -1,20 +1,44 @@
-// // string - string created with single or double quotes works the same.
-// const str1='john  ';
-// const str2="rajat";
-// console.log(typeof str1)    //string
-// console.log(typeof str2)    //string
+// A string in JavaScript is a sequence of characters used to represent text. It can include letters, numbers, symbols, and whitespace.
+// string - string created with single or double quotes works the same.
+const str1='john ';
+const str2="rajat";
+console.log(typeof str1)    //string
+console.log(typeof str2)    //string
 
-// // string properties
-// // length
-// console.log(str1.length);   //4 (including space
 
 // // escape charactor 
 // // (/')- for single quote
 // // (/")- for double quote
 // // (\\)- for back slace
 
-// const str3="hello, my name is \"ram\" ";
-// console.log(str3);
+const str01="hello, my name is \'ram\'";
+console.log(str01);
+
+const str02="hello, my name is \"ram \" ";
+console.log(str02);
+
+const str03="hello, my name is \\ ram ";
+console.log(str03);
+
+// templete string (`)symbol name is backticks
+
+// You can embed variables or expressions using ${...}:
+let str0="john";
+let str00="due";
+console.log(`hello my name is ${str0} and my surname is ${str00}`)   //variables
+
+let a=10;
+let b=20;
+console.log(`sum is : ${a+b}`)  //expressions
+
+let num=5;
+console.log(`5 * ${num} = ${5 * num}`);
+
+// string Methods
+
+// [1] length
+console.log(str1.length);   //5 (including space)
+
 
 // searching element
 // indexof(searchstring)
@@ -28,18 +52,3 @@ console.log(text.lastIndexOf("satyam",15));
 
 // array.from()
 // console.log(Array.from(text));
-
-
-// templete string (`)symbol name is backticks
-
-// You can embed variables or expressions using ${...}:
-let str1="john";
-let str2="due";
-console.log(`hello my name is ${str1} and my surname is ${str2}`)   //variables
-
-let a=10;
-let b=20;
-console.log(`sum is : ${a+b}`)  //expressions
-
-let num=5;
-console.log(`5 * ${num} = ${5 * num}`);
