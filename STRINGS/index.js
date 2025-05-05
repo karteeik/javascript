8915985b23b872b4a6e9f21811e4033acfc39fba
kartikmangukiya <kmtech012@gmail.com>
2025-05-05 23:39:57 +0530

    all setters

--- STRINGS/index.js
+++ STRINGS/index.js
@@ -108,11 +108,48 @@ console.log(text.indexOf("sundaram",1));
 // it's similar to indexOf(), but it searches from the end of the string.
 // lastindexof(searchstring)
 // lastindexof(searchstring,position)
-let str = "banana";
-console.log(str.lastIndexOf("n"));
-console.log(str.lastIndexOf("a", 4)); // 3
+let strr = "banana";
+console.log(strr.lastIndexOf("n"));
+console.log(strr.lastIndexOf("a", 4)); // 3
 // It searches backward from index 4
 //The last "a" before index 4 is at index 3
 
 // array.from()
-// console.log(Array.from(text));
+console.log(Array.from(strr));
+
+//The search() method looks for a match between a string and a regular expression, and returns the index of the first match.
+
+// syntax - string.search(regexp)
+// regexp - A regular expression (can also be a string, but regex is preferred).
+
+// simple regex Examples
+// | Pattern    | Meaning                                    | Matches                 |
+// | ---------- | ------------------------------------------ | ----------------------- |
+// | `a`        | the letter "a"                             | `apple`, `banana`       |
+// | `\d`       | any **digit** (0-9)                        | `1`, `25`, `123abc`     |
+// | `\w`       | any **word character** (a-z, A-Z, 0-9, \_) | `hello_123`             |
+// | `.`        | **any character** (except newline)         | `a`, `1`, `@`           |
+// | `^hello`   | string **starts with** "hello"             | `hello world`           |
+// | `world$`   | string **ends with** "world"               | `this is world`         |
+// | `[aeiou]`  | any **vowel**                              | `a`, `e`, `i`           |
+// | `[A-Z]`    | any **uppercase** letter                   | `G`, `M`, `Z`           |
+// | `\s`       | any **whitespace** (space, tab, newline)   | space, tab              |
+// | `\bword\b` | exact **whole word "word"**                | `word`, not `swordfish` |
+
+
+// special modifier (flag)
+// | Flag | Meaning                   | Example    |
+// | ---- | ------------------------- | ---------- |
+// | `i`  | Case-insensitive          | `/hello/i` |
+// | `g`  | Global (find all matches) | `/a/g`     |
+// | `m`  | Multiline mode            | `/^a/m`    |
+
+
+let searchString = "seven star hotel";
+console.log(searchString.search("Star"));   // -1 bcos, it is case sencetive but, we use
+
+console.log(searchString.search(/Star/i));  // 6 bcos, use of regexp
+console.log(searchString.search(`^seven`)); // 0 
+
+let numString = "there are 123 people of our 2 country";
+console.log(numString.search(/\d+/g));  //10  / \d+ (one and more digits) /
