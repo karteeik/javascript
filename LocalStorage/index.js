691c6f0f0ebda9b4917b13c0fc96dc896983f0fe
kartikmangukiya <kmtech012@gmail.com>
2025-05-25 23:24:39 +0530

    local storage in js

---
+++ LocalStorage/index.js
@@ -0,0 +1,5 @@
+// The localStorage object allows you to save key/value pairs in the browser 
+localStorage.setItem("name","kartik"); 
+localStorage.getItem("name"); 
+localStorage.removeItem("name"); 
+localStorage.clear();
