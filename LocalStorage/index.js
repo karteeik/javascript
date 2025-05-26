d85a7705b2f2440a6e3d8bb8685cc47dce6644ce
kartikmangukiya <kmtech012@gmail.com>
2025-05-26 23:46:15 +0530

    implement

--- LocalStorage/index.js
+++ LocalStorage/index.js
@@ -1,21 +1,28 @@
-// The localStorage object allows you to save key/value pairs in the browser 
-// localStorage.setItem("name","kartik"); 
-// localStorage.getItem("name"); 
-// localStorage.removeItem("name"); 
-// localStorage.clear();
+// // The localStorage object allows you to save key/value pairs in the browser 
+// // localStorage.setItem("name","kartik"); 
+// // localStorage.getItem("name"); 
+// // localStorage.removeItem("name"); 
+// // localStorage.clear();
 
-// JSON Stringify used  to convert json data to string 
-// JSON.stringify(value, replacer, space)
+// // JSON Stringify used  to convert json data to string 
+// // JSON.stringify(value, replacer, space)
 
-const obj = { name: "Alice", age: 25 };
-// const jsonString = JSON.stringify(obj);
+// const obj = { name: "Alice", age: 25 };
+// // const jsonString = JSON.stringify(obj);
+// // console.log(jsonString);
+// // Output: '{"name":"Alice","age":25}'
+// const jsonString = JSON.stringify(obj, null, 2);
 // console.log(jsonString);
-// Output: '{"name":"Alice","age":25}'
-const jsonString = JSON.stringify(obj, null, 2);
+// /* Output:
+// {
+//   "name": "Alice",
+//   "age": 25
+// }
+// */
+const obj = { name: "Alice", age: 25, password: "secret" };
+const jsonString = JSON.stringify(obj, (key, value) => {
+  if (key === "password") return undefined;
+  return value;
+});
 console.log(jsonString);
-/* Output:
-{
-  "name": "Alice",
-  "age": 25
-}
-*/
+// Output: '{"name":"Alice","age":25}'
