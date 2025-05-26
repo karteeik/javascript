91cb233896a8a7236cf12bd34202f375b233f166
kartikmangukiya <kmtech012@gmail.com>
2025-05-26 23:44:12 +0530

    git stringify

--- LocalStorage/index.js
+++ LocalStorage/index.js
@@ -1,8 +1,21 @@
 // The localStorage object allows you to save key/value pairs in the browser 
-localStorage.setItem("name","kartik"); 
-localStorage.getItem("name"); 
-localStorage.removeItem("name"); 
-localStorage.clear();
+// localStorage.setItem("name","kartik"); 
+// localStorage.getItem("name"); 
+// localStorage.removeItem("name"); 
+// localStorage.clear();
 
 // JSON Stringify used  to convert json data to string 
+// JSON.stringify(value, replacer, space)
 
+const obj = { name: "Alice", age: 25 };
+// const jsonString = JSON.stringify(obj);
+// console.log(jsonString);
+// Output: '{"name":"Alice","age":25}'
+const jsonString = JSON.stringify(obj, null, 2);
+console.log(jsonString);
+/* Output:
+{
+  "name": "Alice",
+  "age": 25
+}
+*/
