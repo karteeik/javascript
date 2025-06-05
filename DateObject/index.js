42ddc63e3cc60e6b3caf4db1dbb1c382612c3e5d
kartikmangukiya <kmtech012@gmail.com>
2025-06-05 23:30:18 +0530

    commands

--- DateObject/index.js
+++ DateObject/index.js
@@ -7,8 +7,66 @@
 //     Under the hood, the Date object represents a timestamp
 // the number of milliseconds since January 1, 1970, 00:00:00 UTC (called the Unix Epoch).
 
-const now = new Date(); // current date and time
+// new Date()
+// current date and time ISO 8601 format UTC(Coordinated Universal Time)
+// but this run on console that it returns Indian Standard Time
+const now = new Date(); 
 console.log(now); 
+
+// 3 Types of JavaScript date inout formats
+// ISO Date	"2015-03-25" (The International Standard)
+// Short Date "03/25/2015"
+// Long Date "Mar 25 2015" or "25 Mar 2015"
+
+// | Environment | Output Format   | Timezone | Why Different? |
+// | ----------- | --------------- | -------- | -------------- |
+// | **Node.js** | `toISOString()` | UTC      | Clean for logs |
+// | **Browser** | `toString()`    | Local    | Friendly view  |
+
+const date1 = new Date("2025-02-23");
+console.log(date1);
+
+
+// new Date(date string)
+const dateString = "2025-06-05";
+const dateToString = new Date(dateString);
+console.log(dateToString);
+
+let dateStr = "October 13,2023";
+let dateToStr = new Date(dateStr);
+console.log(dateToStr);
+
+
+// new Date(year,month)
+const yearMonth = new Date(2025,5);  //if you passed in Date constructor than first is year and second is month 0 to 11
+console.log(yearMonth);
+
+
+// new Date(year,month,day)
+const yMD = new Date(2025,11,4); 
+console.log(yMD);
+
+// new Date(year,month,day,hours)
+const yearToHours = new Date(2025, 3, 5, 11);
+console.log(yearToHours);
+
+// new Date(year,month,day,hours,minutes)
+const yearToMinutes = new Date(2025, 3, 5, 11, 32);
+console.log(yearToMinutes);
+
+// new Date(year,month,day,hours,minutes,seconds)
+const yearToSecond = new Date(2025, 3, 5, 11, 32, 56);
+console.log(yearToSecond);
+
+// new Date(year,month,day,hours,minutes,seconds,ms)
+const yearToMS = new Date(2025, 3, 5, 11, 32, 56, 3432);
+console.log(yearToMS);
+
+// new Date(milliseconds)
+// One day (24 hours) is 86 400 000 milliseconds.
+
+const dateMS = new Date(100000000000);
+console.log(dateMS);
 
 const d1 = new Date("2025-06-04"); // ISO format (YYYY-MM-DD)
 console.log(d1.toString());
