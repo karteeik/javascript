eab1a8555a81da3b4dd41779c5be2a2581f93e6b
kartikmangukiya <kmtech012@gmail.com>
2025-06-06 23:51:31 +0530

    work with timing based events

--- TimingBasedEvents/index.js
+++ TimingBasedEvents/index.js
@@ -1,6 +1,20 @@
-// List of Timing Based Events
-// setTimeout()
-// setinterval()
-// clearTimeout()
-// clearInterval()
+// JavaScript is single-threaded and uses a mechanism called the event loop to handle asynchronous operations like setTimeout() and setInterval(). These timing functions allow you to schedule code to run: Once after a delay → setTimeout() / Repeatedly at intervals → setInterval()
 
+// List of Timing Based Events
+// setTimeout() - Run Code After Delay
+// setinterval() - Run Repeatedly at Intervals
+// clearTimeout() - Cancel a Timeout
+// clearInterval() - Cancel an Interval 
+
+// setTimeout() - Run Code After Delay
+// Syntax - setTimeout(callback, delay, ...args);
+
+// clearTimeout() - Cancel a Timeout
+
+
+// setinterval() - Run Repeatedly at Intervals
+// Syntax - setInterval(callback, interval, ...args);
+
+// clearInterval() - Cancel an Interval 
+
+// Write a Function to Show 3 Seconds Countdown Timer
