f5213112c0a3b8c47bad871e3352f3e2f75f4a67
kartikmangukiya <kmtech012@gmail.com>
2025-06-08 23:33:33 +0530

    Digital Watch project

--- TimingBasedEvents/index.js
+++ TimingBasedEvents/index.js
@@ -6,31 +6,52 @@
 // clearTimeout() - Cancel a Timeout
 // clearInterval() - Cancel an Interval 
 
-// setTimeout() - Run Code After Delay
+// 1 - setTimeout() - Run Code After Delay
 // Syntax - setTimeout(callback, delay, ...args);
-// function Delayfunction(){
-//     console.log("Function Runs After 2 Seconds");
-// };
-// setTimeout(Delayfunction,2000);     //1 Seconds = 1000 MilliSeconds
+// Uses - Hide popup
+function Delayfunction(){
+    console.log("Function Runs After 2 Seconds");
+};
+setTimeout(Delayfunction,2000);     //1 Seconds = 1000 MilliSeconds
 
-// clearTimeout() - Cancel a Timeout
-// clearTimeout(Delayfunction());
-
-// setinterval() - Run Repeatedly at Intervals
+// 2 - setinterval() - Run Repeatedly at Intervals
 // Syntax - setInterval(callback, interval, ...args);
+// Uses - Dashboard polling
 setInterval(() => {
   console.log("This runs every 1 second");
 }, 1000);
 
+// clearTimeout() - Cancel a Timeout
+// Syntax - clearTimeout(timeoutID);
+// Uses - Cancel auto-logout 
+function clrTime(){
+  console.log("clear setTimeout functionality");
+}
+const clrTimeID = setTimeout(clrTime,1000);
+clearTimeout(clrTimeID);
+
 // clearInterval() - Cancel an Interval 
-const interval = setInterval(() => {
-  console.log("Repeating...");
-}, 1000);
+// Syntax - clearInterval(intervalID);
+// Uses - Countdown timer
+function intervalFun(){
+  console.log("This Function Repeat Every 1 Second");
+};
+intervalFun();
 
-setTimeout(() => {
-  clearInterval(interval); // Stops after 5 seconds
-  console.log("Stopped repeating");
-}, 5000);
-
+const intervalID = setInterval(intervalFun,1000);
+setTimeout(()=>{
+  clearInterval(intervalID);
+},5000);
 
 // Write a Function to Show 3 Seconds Countdown Timer
+
+let second = 5;
+let timer = setInterval(() => {
+  console.log(`Your Time is ${second} Left`);
+  second--;
+
+  if(second<=0){
+    clearInterval(timer);
+    console.log('Time is up');
+  }
+},1000);
