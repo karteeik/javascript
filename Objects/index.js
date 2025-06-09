ad62d9066d8a2245626e980f9d9a5576228388e5
kartikmangukiya <kmtech012@gmail.com>
2025-06-09 23:58:53 +0530

    Object Topic Started.....

--- Objects/index.js
+++ Objects/index.js
@@ -1,1 +1,10 @@
-const obj = {}
+// JavaScript Object - An object is a collection of key-value pairs
+// The key is always a string (or symbol).
+// The value can be any data type: string, number, array, function, another object, etc.
+// Syntax - const obj = {};
+
+let person = {
+    name: "Kartik",
+    age: 25,
+    isDeveloper: true
+};
