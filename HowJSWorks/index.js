08bc9e0acde760701611263f982f2cfeaa375ecf
kartikmangukiya <kmtech012@gmail.com>
2025-06-14 23:54:22 +0530

    work flow of js

--- HowJSWorks/index.js
+++ HowJSWorks/index.js
@@ -1,1 +1,8 @@
-console.log(globalThis);
+//  Component     What It Does?                                 
+//  -----------  -------------------------------------------- 
+//  JS Engine     Runs your code (V8, SpiderMonkey, etc.)      
+//  Call Stack    Tracks function execution                    
+//  Memory Heap   Stores variables/objects                     
+//  Web APIs      Handle async tasks (e.g., setTimeout)        
+//  Event Loop    Coordinates async callback execution         
+//  Task Queues   Microtask (Promises), Macrotask (setTimeout) 
