5e1a2935d59db0b4476626cf867e579ac84ec038
kartikmangukiya <kmtech012@gmail.com>
2025-06-15 23:54:39 +0530

    Different phases

--- HowJSWorks/index.js
+++ HowJSWorks/index.js
@@ -1,10 +1,33 @@
-//  Component     What It Does?                                 
-//  -----------  -------------------------------------------- 
-//  JS Engine     Runs your code (V8, SpiderMonkey, etc.)      
-//  Call Stack    Tracks function execution                    
-//  Memory Heap   Stores variables/objects                     
-//  Web APIs      Handle async tasks (e.g., setTimeout)        
-//  Event Loop    Coordinates async callback execution         
-//  Task Queues   Microtask (Promises), Macrotask (setTimeout) 
+// How JS Works..............
 
-// Your Code → JavaScript Engine → Call Stack + Memory Heap → Web APIs → Task Queues (Microtask & Macrotask) → Event Loop → Call Stack (again!)
+// Our JavaScript Code -->
+
+// Parsing Phase (by JavaScript Engine) -->
+// Step 1: Lexical Analysis (Tokenizing)
+//        JS code is split into meaningful tokens.
+
+// Step 2: Syntax Parsing
+//        Tokens are checked for syntax errors.
+//        Then, an Abstract Syntax Tree (AST) is created.
+
+// Compilation Phase -->
+//        Using JIT (Just-In-Time) Compiler,
+//        AST is converted into Intermediate Code or
+//        optimized Machine Code (via Bytecode in modern engines like V8).
+
+// Execution Phase -->
+// Execution Phase - In Execution Phase, We Have Call Stack With 
+
+// (1) Creation Phase -->
+// Memory is allocated for variables and functions.
+// Functions are hoisted.
+// Variables are hoisted and set to undefined.
+
+// (2) Execution Contexts -->
+// Code is executed line-by-line.
+// Values are assigned to variables.
+// Functions are invoked.
+
+// (i) Call Stack - Data Structure LIFO(pushed,popped), Stack Overflow
+// (ii) Heap Memory - An area in Memory used to store Objects, Arrays and Functions.
+
