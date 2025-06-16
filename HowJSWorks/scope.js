944182f7bd44f4552c2530ed9e2ba3faf2ccec5c
kartikmangukiya <kmtech012@gmail.com>
2025-06-16 23:57:47 +0530

    All set JS works

--- HowJSWorks/scope.js
+++ HowJSWorks/scope.js
@@ -2,3 +2,13 @@
 
 // 1 - Global Scope
 // Declared outside of any function or block & Accessible everywhere in your code.
+
+// 2 - Function Scope
+// Variables declared inside a function are only accessible inside that function.
+
+// 3 - Block Scope (ES6+)
+// introduced with let and const.
+// Variables declared inside {} are only accessible inside that block.
+
+// 4 - Lexical Scope (Static Scope)
+// A function can access variables from its parent scope, even if it's nested.
