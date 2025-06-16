75f76a34d9794f5742e30f2f19b1d498a9b86be9
kartikmangukiya <kmtech012@gmail.com>
2025-06-16 23:55:03 +0530

    Scoping

--- HowJSWorks/scope.js
+++ HowJSWorks/scope.js
@@ -1,1 +1,4 @@
 // Scope - Scope determines where variables, functions, and objects are accessible in your code.
+
+// 1 - Global Scope
+// Declared outside of any function or block & Accessible everywhere in your code.
