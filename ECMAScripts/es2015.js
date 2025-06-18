d83da9601619eebd343873b179ce53ac29074492
kartikmangukiya <kmtech012@gmail.com>
2025-06-18 23:53:03 +0530

    ECMAScript2015 implements

--- ECMAScripts/es2015.js
+++ ECMAScripts/es2015.js
@@ -1,1 +1,17 @@
-// The Object
+// The Object properties - Modern Javascript
+
+const name = "john";
+const age = 21;
+
+// Object
+const person = {name, age};
+// is work like that const person = { name: name, age: age};
+console.log(person);
+
+// Destructuring in JavaScript
+const numbers = [10,20,30];
+const [first,second,third] = numbers;
+console.log(first);
+console.log(second);
+console.log(third);
+
