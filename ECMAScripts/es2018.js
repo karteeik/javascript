001985356ddc0043fa8f1d4231f9a91312362b7d
kartikmangukiya <kmtech012@gmail.com>
2025-06-20 23:31:34 +0530

    Above function is it

--- ECMAScripts/es2018.js
+++ ECMAScripts/es2018.js
@@ -1,3 +1,5 @@
 // ES2018 brought useful features focused on asynchronous patterns, regular expressions, and object manipulation.
 
 // 1. Asynchronous Iteration with for await...of
+//  2. Rest/Spread Properties for Objects
+// 3. Promise.prototype.finally()
