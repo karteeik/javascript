8822a2e166cd5b44dce59bff8794ed76b71e8aa9
kartikmangukiya <kmtech012@gmail.com>
2025-06-21 23:44:43 +0530

    ECMAScript 2019 implements

---
+++ ECMAScripts/es2019.js
@@ -0,0 +1,9 @@
+// ECMAScript 2019 introduced useful tools for working with arrays, strings, objects, and optional error handling
+
+// 1. Array.prototype.flat()
+// 2. Array.prototype.flatMap()
+// 3. Object.fromEntries()
+// 4. String.prototype.trimStart() / trimEnd()
+// 5. Optional catch binding
+// 6. Symbol.prototype.description
+// 7. Stable Array.sort()
