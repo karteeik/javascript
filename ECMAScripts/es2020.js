6b1db66891bddbe3b8800af1eb1ead911e8e51b9
kartikmangukiya <kmtech012@gmail.com>
2025-06-21 23:46:12 +0530

    ECMAScript 2020 implements

---
+++ ECMAScripts/es2020.js
@@ -0,0 +1,9 @@
+// ES2020 added powerful tools for optional chaining, null checks, big integers, and more. These features make code safer, shorter, and cleaner.
+
+// 1. Optional Chaining (?.)
+//  2. Nullish Coalescing Operator (??)
+// 3. BigInt – Work with really big numbers
+// 4. Promise.allSettled()
+// 5. globalThis
+// 6. dynamic import()
+// 7. matchAll() for global regex
