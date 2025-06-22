5f9caca289a133cfadcdc7c11e4742d3690cdfce
kartikmangukiya <kmtech012@gmail.com>
2025-06-22 23:59:25 +0530

    df

--- ECMAScripts/es2020.js
+++ ECMAScripts/es2020.js
@@ -22,11 +22,32 @@ console.log(age ?? 18);   // 0  (only checks for null or undefined)
 // Represents integers larger than Number.MAX_SAFE_INTEGER.
 
 const big = 1234567890123456789012345678901234567890n;
-
 console.log(big + 10n); // Works 
 
 // 4. Promise.allSettled()
+// Waits for all promises to complete — whether fulfilled or rejected.
+
+const tasks = [
+  Promise.resolve("Success"),
+  Promise.reject("Fail"),
+];
+
+Promise.allSettled(tasks).then(results => {
+  console.log(results);
+});
+
 // 5. globalThis
+// Provides a universal global object that works in any environment (browser, Node, etc.).
+console.log(globalThis); // Window in browser, global in Node.js
+
 // 6. dynamic import()
+// Load JS modules dynamically (lazy-loading).
+
+button.addEventListener("click", async () => {
+  const module = await import("./math.js");
+  console.log(module.add(2, 3));
+});
+
 // 7. matchAll() for global regex
+
 // 8. Private Class Fields (#)
