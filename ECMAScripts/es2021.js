1a6a9f178ad2e865e38fa2d2bea7e18258c43d5e
kartikmangukiya <kmtech012@gmail.com>
2025-06-23 23:59:16 +0530

    ECMAScript new logical operaters

--- ECMAScripts/es2021.js
+++ ECMAScripts/es2021.js
@@ -1,4 +1,4 @@
-// String.prototype.replaceAll()
+// 1. String.prototype.replaceAll()
 // A new method to replace all instances of a substring without needing a regular expression.
 
 const str1 = "hello john, hello again";
@@ -9,5 +9,5 @@ console.log(str1.replaceAll("hello","hi"));
 // No need for regex.
 // Safer and more readable.
 
-
-
+// 2. Logical Assignment Operators (&&=, ||=, ??=)
+// These combine logical operators (&&, ||, ??) with assignment.
