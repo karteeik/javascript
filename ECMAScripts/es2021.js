31e0c34860afa70a4f76da2a416688c535ac4518
kartikmangukiya <kmtech012@gmail.com>
2025-06-23 23:58:10 +0530

    revise

--- ECMAScripts/es2021.js
+++ ECMAScripts/es2021.js
@@ -2,8 +2,12 @@
 // A new method to replace all instances of a substring without needing a regular expression.
 
 const str1 = "hello john, hello again";
-console.log(str1.replaceAll(/hello/g,"hi"));
+// console.log(str1.replace(/hello/g,"hi"));
 
-// console.log(str1.replaceAll("hello","hi"));
-const msg = "foo-bar-foo";
-console.log(msg.replace(/foo/g, "baz")); // baz-bar-baz
+console.log(str1.replaceAll("hello","hi"));
+//  Why it's useful:
+// No need for regex.
+// Safer and more readable.
+
+
+
