31c9018d514f77df0e29a6bcb31d2b8516d5557f
kartikmangukiya <kmtech012@gmail.com>
2025-06-27 23:58:35 +0530

    DSA with js

--- AdvanceJS/CallbackHell.js
+++ AdvanceJS/CallbackHell.js
@@ -1,5 +1,14 @@
 // Callback Hell happens when you nest too many callback functions inside each other.
 // This situation is refers to asyncronous javascript programming
 
+functionA(function() {
+  functionB(function() {
+    functionC(function() {
+      functionD(function() {
+        // 😵 you get lost here!
+      });
+    });
+  });
+});
 
 // This creates a "pyramid" or "staircase" shape in your code → hard to read, debug, or maintain.
