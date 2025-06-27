5b28598c260188ba61e73729e5d133ca2aa553db
kartikmangukiya <kmtech012@gmail.com>
2025-06-27 23:56:23 +0530

    CallBack Hell Topic Implement

--- AdvanceJS/CallbackHell.js
+++ AdvanceJS/CallbackHell.js
@@ -1,5 +1,5 @@
 // Callback Hell happens when you nest too many callback functions inside each other.
-
+// This situation is refers to asyncronous javascript programming
 
 
 // This creates a "pyramid" or "staircase" shape in your code → hard to read, debug, or maintain.
