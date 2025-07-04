d3d83f9e99353c710569fe923a58d7ef78a8c34d
kartikmangukiya <kmtech012@gmail.com>
2025-07-04 22:44:16 +0530

    JavaScript revision

--- 4_ConcatAndTypeOfCoerion/index.js
+++ 4_ConcatAndTypeOfCoerion/index.js
@@ -4,14 +4,14 @@
 // some quiz
 console.log("java"+"script")    //This is concatenation in js using + sign
 console.log("java"-"script") //Nan
-console.log(16+4+"java")
+console.log(16+4+"java")    // String + Number → String
 console.log(true+true)
 console.log(true+false)
 console.log(false-true)
 console.log(true-false)
 console.log(""+0)
 console.log(""+"")
-console.log(9-"5")
+console.log(9-"5")  // Number - String → Number
 console.log(10+9+"john")
 console.log("34"-6)
 console.log("34"+6)  //implicit
@@ -35,4 +35,5 @@ let int = parseInt("123", 10);  // explicit
 console.log(int);
 
 console.log(null == undefined);  // true
-console.log('0' == 0);  // true
+console.log('0' == 0);  // true - because of coercion
+console.log('0' === 0);  // false - different types
